2bd37a6b0dfa17577335a06be570dff40003805c
Eric Ouyang <contact@ericouyang.com>
2013-12-03 16:09:22 -0500

    authentication initial implementation

--- Recipes/src/net/cs50/recipes/SyncUtils.java
+++ Recipes/src/net/cs50/recipes/SyncUtils.java
@@ -18,6 +18,9 @@ package net.cs50.recipes;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
@@ -44,6 +47,7 @@ public class SyncUtils {
         boolean setupComplete = PreferenceManager
                 .getDefaultSharedPreferences(context).getBoolean(PREF_SETUP_COMPLETE, false);
 
+        /*
         // Create account, if it's missing. (Either first run, or user has deleted account.)
         Account account = AccountService.GetAccount();
         AccountManager accountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
@@ -58,7 +62,28 @@ public class SyncUtils {
                     account, CONTENT_AUTHORITY, new Bundle(),SYNC_FREQUENCY);
             newAccount = true;
         }
+		*/
+        AccountManager accountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
+        
+        final AccountManagerFuture<Bundle> future = accountManager.addAccount(AccountService.ACCOUNT_TYPE, AccountService.AUTH_TOKEN_TYPE, null, null, (Activity) context, new AccountManagerCallback<Bundle>() {
+            @Override
+            public void run(AccountManagerFuture<Bundle> future) {
+                try {
+                    Bundle bnd = future.getResult();
+                    TriggerRefresh();
+                    
+                    //showMessage("Account was created");
+                    //Log.d("udinic", "AddNewAccount Bundle is " + bnd);
 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ///showMessage(e.getMessage());
+                }
+            }
+        }, null);
+        
+        //PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean(PREF_SETUP_COMPLETE, true).commit();
+        /*
         // Schedule an initial sync if we detect problems with either our account or our local
         // data has been deleted. (Note that it's possible to clear app data WITHOUT affecting
         // the account list, so wee need to check both.)
@@ -67,6 +92,7 @@ public class SyncUtils {
             PreferenceManager.getDefaultSharedPreferences(context).edit()
                     .putBoolean(PREF_SETUP_COMPLETE, true).commit();
         }
+        */
     }
 
     /**
