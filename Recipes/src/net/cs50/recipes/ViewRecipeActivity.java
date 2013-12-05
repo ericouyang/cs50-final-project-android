da2c281c7510e1ea2053eaeb79b091c0b2c63cdf
michaelhwang <michaelhwang31@gmail.com>
2013-12-05 02:43:47 -0500

    inprogress recipe view

--- Recipes/src/net/cs50/recipes/ViewRecipeActivity.java
+++ Recipes/src/net/cs50/recipes/ViewRecipeActivity.java
@@ -1,27 +1,84 @@
 package net.cs50.recipes;
 
 import net.cs50.recipes.provider.RecipeContract;
+import net.cs50.recipes.types.Recipe;
 import net.cs50.recipes.util.ImageHelper;
+import net.cs50.recipes.util.RecipeHelper;
 import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 
 public class ViewRecipeActivity extends Activity {
-
-    private Cursor mCursor;
+	
+	final Context context = this;
+    //private Cursor mCursor;
     private TextView mRecipeNameView;
     private ImageView mRecipeImageView;
-
+    private Button commentButton;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
+        
+        /*
+        commentButton = (Button) findViewById(R.id.buttonAlert);
+        
+		// add button listener
+		commentButton.setOnClickListener(new OnClickListener() {
+ 
+		@Override
+		public void onClick(View arg0) {
+ 
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				context);
+ 
+			// set title
+			alertDialogBuilder.setTitle("Your Title");
+ 
+			// set dialog message
+			alertDialogBuilder
+				.setMessage("Click yes to exit!")
+				.setCancelable(false)
+				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// if this button is clicked, close
+						// current activity
+						ViewRecipeActivity.this.finish();
+					}
+				  })
+				.setNegativeButton("No",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// if this button is clicked, just close
+						// the dialog box and do nothing
+						dialog.cancel();
+					}
+				});
+ 
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+ 
+				// show it
+				alertDialog.show();
+			}
+
+		
+		});
+        */
+        
         Uri resUri = getIntent().getData();
 
+        /*
         mCursor = getContentResolver().query(
                 resUri,
                 RecipeContract.Recipe.PROJECTION_ALL_FIELDS,
@@ -29,15 +86,17 @@ public class ViewRecipeActivity extends Activity {
                 null,
                 null);// The sort order for the returned rows
 
+		mCursor.moveToFirst();
+		*/
+        
         mRecipeNameView = (TextView) findViewById(R.id.view_recipe_name);
         mRecipeImageView = (ImageView) findViewById(R.id.view_recipe_image);
 
-        mCursor.moveToFirst();
+        Recipe recipe = RecipeHelper.getRecipe(resUri, this);
+        
+        mRecipeNameView.setText(recipe.getName());
 
-        mRecipeNameView.setText(mCursor.getString(RecipeContract.Recipe.PROJECTION_ALL_FIELDS_COLUMN_NAME));
-
-        String imageUrl = mCursor.getString(RecipeContract.Recipe.PROJECTION_ALL_FIELDS_COLUMN_PRIMARY_IMAGE_URL);
-        ImageHelper.loadBitmap(imageUrl, mRecipeImageView);
+        ImageHelper.loadBitmap(recipe.getImage(0), mRecipeImageView);
     }
 
 
