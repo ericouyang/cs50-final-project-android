6ccae2ca9a955339f277bbc18bdddeef0057e1b4
michaelhwang <michaelhwang31@gmail.com>
2013-12-05 23:57:41 -0500

    add comment button

--- Recipes/src/net/cs50/recipes/ViewRecipeActivity.java
+++ Recipes/src/net/cs50/recipes/ViewRecipeActivity.java
@@ -1,5 +1,7 @@
 package net.cs50.recipes;
 
+import java.util.List;
+
 import net.cs50.recipes.provider.RecipeContract;
 import net.cs50.recipes.types.Recipe;
 import net.cs50.recipes.util.ImageHelper;
@@ -9,10 +11,13 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,84 +28,50 @@ import android.app.AlertDialog;
 public class ViewRecipeActivity extends Activity {
 	
 	final Context context = this;
-    //private Cursor mCursor;
     private TextView mRecipeNameView;
     private ImageView mRecipeImageView;
     private TextView mRecipeUserName;
-    
+    private TextView mRecipeNoms;
+    private ListView mRecipeComments;
+    private TextView mRecipeCreatedAt;
+    private ListView mRecipeIngredients;
+    private ListView mRecipeInstructions;
+    private Recipe recipe;
     private Button commentButton;
+    
+    
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
         
-        /*
-        commentButton = (Button) findViewById(R.id.buttonAlert);
-        
-		// add button listener
-		commentButton.setOnClickListener(new OnClickListener() {
- 
-		@Override
-		public void onClick(View arg0) {
- 
-			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-				context);
- 
-			// set title
-			alertDialogBuilder.setTitle("Your Title");
- 
-			// set dialog message
-			alertDialogBuilder
-				.setMessage("Click yes to exit!")
-				.setCancelable(false)
-				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,int id) {
-						// if this button is clicked, close
-						// current activity
-						ViewRecipeActivity.this.finish();
-					}
-				  })
-				.setNegativeButton("No",new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog,int id) {
-						// if this button is clicked, just close
-						// the dialog box and do nothing
-						dialog.cancel();
-					}
-				});
- 
-				// create alert dialog
-				AlertDialog alertDialog = alertDialogBuilder.create();
- 
-				// show it
-				alertDialog.show();
-			}
-
-		
-		});
-        */
-        
         Uri resUri = getIntent().getData();
-
-        /*
-        mCursor = getContentResolver().query(
-                resUri,
-                RecipeContract.Recipe.PROJECTION_ALL_FIELDS,
-                null,
-                null,
-                null);// The sort order for the returned rows
-
-		mCursor.moveToFirst();
-		*/
         
         mRecipeNameView = (TextView) findViewById(R.id.view_recipe_name);
         mRecipeImageView = (ImageView) findViewById(R.id.view_recipe_image);
         mRecipeUserName = (TextView) findViewById(R.id.view_recipe_user_name);
+        mRecipeNoms = (TextView) findViewById(R.id.view_recipe_noms);
+        mRecipeComments = (ListView) findViewById(R.id.view_recipe_comments);
+        mRecipeCreatedAt = (TextView) findViewById(R.id.view_recipe_created_at);
+        mRecipeIngredients = (ListView) findViewById(R.id.view_recipe_ingredients);
+        mRecipeInstructions = (ListView) findViewById(R.id.view_recipe_instructions);
         
-        Recipe recipe = RecipeHelper.getRecipe(resUri, this);
+        recipe = RecipeHelper.getRecipe(resUri, this);
         
         mRecipeNameView.setText(recipe.getName());
-        mRecipeUserName.setText("Fred!");
+        mRecipeUserName.setText("Fred!"); /*
+        mRecipeNoms.setText(recipe.getName());
+        mRecipeComments.setText(recipe.getComments());
+        
+        long createdAt = recipe.getCreatedAt();
+        mRecipeCreatedAt.setText(String.valueOf(createdAt));
+        
+        List<String> ingredients = recipe.getIngredients();
+        mRecipeIngredients.setText(recipe.getIngredients());
+        
+        List<String> instructions = recipe.getInstructions();
+        mRecipeInstructions.setText(recipe.getInstructions()); */
         
         ImageHelper.loadBitmap(recipe.getImage(0), mRecipeImageView);
     }
@@ -110,6 +81,57 @@ public class ViewRecipeActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_view_recipe, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       
+        // Handle action buttons
+        switch(item.getItemId()) {
+        
+        case R.id.menu_comment:
+        	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+    				context);
+     
+    			// set title
+    			//alertDialogBuilder.setTitle("Comment on Recipe");
+    			
+    			// Use an EditText view to get user input.
+    	         final EditText input = new EditText(this);
+    	         input.setId(0);
+    	         alertDialogBuilder.setView(input);
+    	         
+    	         
+    			// set dialog message
+    			alertDialogBuilder
+    				.setMessage("Comment on recipe:")
+    				.setCancelable(false)
+    				.setPositiveButton("Send",new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog,int id) {
+    						// if this button is clicked, close
+    						// current activity
+    						recipe.addComment("test comment");
+    						ViewRecipeActivity.this.finish();
+    					}
+    				  })
+    				.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog,int id) {
+    						// if this button is clicked, just close
+    						// the dialog box and do nothing
+    						dialog.cancel();
+    					}
+    				});
+     
+    				// create alert dialog
+    				AlertDialog alertDialog = alertDialogBuilder.create();
+     
+    				// show it
+    				alertDialog.show();
+    				return true;
+    		
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 
